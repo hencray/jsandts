185f05993e8b185685bb251c6b51b1ad37ec2792
Henry Martija <henrymartija@gmail.com>
2024-04-10 19:19:44 -0700

    Section 1 Done
    ✨ (script.js, scripts.ts): add scripts for basic arithmetic operations and type examples in TypeScript to better understand variable types and coercion

---
+++ section-1/scripts.ts
@@ -0,0 +1,66 @@
+// // declare a variable with type string
+// const stringVariable: string = "test";
+
+// // declare a variable that can either be a tsring or a number
+// const stringOrNum: string | number = "test";
+// console.log(stringOrNum);
+
+// // declare a variable that can either be a string or null
+// const stringOrNull: string | null = "test";
+
+// // declare a unitialized variable with type of string
+// let a: string | undefined;
+
+// const x = 5;
+// const y = 10;
+// const sum = x + y;
+// sum;
+// const difference = x - y;
+// difference;
+// const product = x * y;
+// product;
+// const quotient = x / y;
+// quotient;
+// const remainder = x % y;
+// remainder;
+// const power = x ** y;
+// power;
+// const bitwiseXOR = x ^ y;
+// bitwiseXOR;
+
+// // you cant do these on not of type number
+// const nan = "asd" + 5;
+// nan;
+// console.log(power);
+// // coercion is terning one datatype to another data type
+// const l: string = `${sum} ${power}`;
+// console.log(l);
+
+// // ways to coerce a value to a number
+// const str = "5";
+// const number = +"5";
+// console.log(number);
+
+// // parseInt
+// const number2 = parseInt("5234.1asd")
+// number2
+// const number3 = parseFloat("543.123asd")
+// number3
+
+// // Number
+// const number4 = Number("444.4");
+// number4
+
+// // Infinity -> number
+// console.log(Infinity);
+// console.log(-Infinity);
+
+// compound assignment operations
+// let x = 5;
+// x;
+// x += 5;
+// x;
+
+// increment the number 1
+let x = 1;
+x += 1;
